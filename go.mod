28e08e2f182f6b0d0852422ef7410e1c1ed7afd1
Ask Bjørn Hansen <ask@develooper.com>
2023-12-03 23:14:48 -0800

    Add tracing

--- go.mod
+++ go.mod
@@ -1,40 +1,66 @@
 module go.askask.com/screensnap
 
-go 1.20
+go 1.21.3
+
+toolchain go1.21.4
 
 require (
 	github.com/chromedp/cdproto v0.0.0-20231205062650-00455a960d61
 	github.com/chromedp/chromedp v0.9.3
 	github.com/labstack/echo/v4 v4.11.3
 	github.com/prometheus/client_golang v1.17.0
+	github.com/samber/slog-echo v1.9.0
+	go.ntppool.org/common v0.2.5
+	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.46.1
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1
+	go.opentelemetry.io/otel v1.21.0
+	go.opentelemetry.io/otel/trace v1.21.0
 	golang.org/x/exp v0.0.0-20231127185646-65229373498e
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.3.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/remychantenay/slog-otel v1.2.3 // indirect
+	github.com/samber/lo v1.39.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.21.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231127180814-3a041ad873d4 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231127180814-3a041ad873d4 // indirect
+	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -12,6 +12,13 @@ import (
 	"net/http/pprof"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	slogecho "github.com/samber/slog-echo"
+	"go.ntppool.org/common/logger"
+	"go.ntppool.org/common/tracing"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
@@ -29,9 +36,21 @@ func (m *httpErr) Error() string {
 }
 
 func main() {
+	log := logger.Setup()
+
+	srvContext := context.Background()
+
+	tpShutdown, err := tracing.InitTracer(srvContext, &tracing.TracerConfig{
+		ServiceName: "screensnap",
+	})
+	if err != nil {
+		log.ErrorContext(srvContext, "tracing setup", "err", err)
+		os.Exit(2)
+	}
+
 	upstream := os.Getenv("upstream_base")
 	if len(upstream) == 0 {
-		slog.Error("ntppool upstream_base not set")
+		log.ErrorContext(srvContext, "ntppool upstream_base not set")
 		time.Sleep(1 * time.Second)
 		os.Exit(2)
 	}
@@ -42,7 +61,7 @@ func main() {
 		chromedp.DisableGPU,
 	)
 	// create chromedp's context
-	parentCtx, execCancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	parentCtx, execCancel := chromedp.NewExecAllocator(srvContext, opts...)
 	defer execCancel()
 
 	// create a new browser
@@ -51,7 +70,7 @@ func main() {
 
 	// start the browser without a timeout
 	if err := chromedp.Run(browserCtx); err != nil {
-		slog.Error("start browser (chromedp.Run)", "err", err)
+		log.ErrorContext(srvContext, "start browser (chromedp.Run)", "err", err)
 		os.Exit(10)
 	}
 
@@ -67,16 +86,59 @@ func main() {
 	}()
 
 	e := echo.New()
+	e.Use(otelecho.Middleware("screensnap"))
+	e.Use(slogecho.NewWithConfig(log,
+		slogecho.Config{
+			WithTraceID: true,
+			WithSpanID:  true,
+			// WithRequestHeader: true,
+		},
+	))
+	e.Use(echo.WrapMiddleware(
+		otelhttp.NewMiddleware("screensnap",
+			otelhttp.WithMessageEvents(otelhttp.ReadEvents, otelhttp.WriteEvents),
+		),
+	))
+
+	e.Use(
+		func(next echo.HandlerFunc) echo.HandlerFunc {
+			return func(c echo.Context) error {
+				request := c.Request()
+
+				span := trace.SpanFromContext(request.Context())
+				c.Response().Header().Set("Traceparent", span.SpanContext().TraceID().String())
+
+				return next(c)
+			}
+		},
+	)
+
 	e.Add("GET", "/image/offset/:ip", offsetHandler(parentCtx, upstream), middleware.Logger())
 	e.Add("GET", "/__health", func(c echo.Context) error {
 		// todo: check that the browser actually works
 		return c.String(200, "ok")
 	})
 
-	err := e.Start(":8000")
+	go func() {
+		err := e.Start(":8000")
+		if err != nil {
+			log.Error("echo Start", "err", err)
+		}
+	}()
+
+	<-srvContext.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(srvContext, 15*time.Second)
+	defer cancel()
+	if err := e.Shutdown(shutdownCtx); err != nil {
+		log.Warn("could not shutdown echo", "err", err)
+	}
+
+	shutdownCtx, tpCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer tpCancel()
+	err = tpShutdown(shutdownCtx)
 	if err != nil {
-		slog.Error("echo Start", "err", err)
-		os.Exit(2)
+		log.Warn("could not shutdown tracer", "err", err)
 	}
 
 }
@@ -86,7 +148,7 @@ func offsetHandler(mainCtx context.Context, upstream string) func(echo.Context) 
 		ipStr := c.Param("ip")
 		slog.Info("offsetHandler", "ip", ipStr)
 
-		buf, err := takeScreenshot(mainCtx, upstream, ipStr)
+		buf, err := takeScreenshot(mainCtx, c.Request().Context(), upstream, ipStr)
 		if err != nil {
 			var hErr *httpErr
 			if errors.As(err, &hErr) {
@@ -101,7 +163,13 @@ func offsetHandler(mainCtx context.Context, upstream string) func(echo.Context) 
 	}
 }
 
-func takeScreenshot(mainCtx context.Context, upstream, ip string) ([]byte, error) {
+func takeScreenshot(mainCtx, reqCtx context.Context, upstream, ip string) ([]byte, error) {
+
+	log := logger.Setup()
+	reqCtx, span := tracing.Tracer().Start(
+		reqCtx, "takeScreenshot",
+	)
+	defer span.End()
 
 	ctx, cancel := chromedp.NewContext(
 		mainCtx,
@@ -110,8 +178,10 @@ func takeScreenshot(mainCtx context.Context, upstream, ip string) ([]byte, error
 	defer cancel()
 
 	defer func() {
+		span.AddEvent("closing tab")
 		if err := chromedp.Run(ctx, page.Close()); err != nil {
-			slog.Error("could not close tab", "err", err)
+			log.ErrorContext(reqCtx, "could not close tab", "err", err)
+			span.RecordError(err)
 		}
 	}()
 
@@ -124,31 +194,47 @@ func takeScreenshot(mainCtx context.Context, upstream, ip string) ([]byte, error
 	// retina / hidpi / 2x screenshot
 	// emulateOpts := chromedp.EmulateScale(2)
 
+	_, spanRun := tracing.Tracer().Start(
+		reqCtx, "chromedp.RunResponse",
+		trace.WithAttributes(attribute.String("url", url)),
+	)
 	resp, err := chromedp.RunResponse(ctx, chromedp.Tasks{
 		chromedp.Navigate(url),
 		chromedp.EmulateViewport(int64(viewY), int64(viewX)), // emulateOpts),
 	})
 	if err != nil {
+		spanRun.RecordError(err)
+		spanRun.End()
 		return nil, err
 	}
 	if resp.Status != 200 {
 		if resp.Status == 404 {
+			spanRun.End()
 			return nil, &httpErr{status: 404}
 		}
-		slog.Info("invalid response status", "status", resp.Status, "url", url)
-		return nil, fmt.Errorf("response status %d", resp.Status)
+		err := fmt.Errorf("response status %d", resp.Status)
+		spanRun.RecordError(err)
+		log.WarnContext(reqCtx, "invalid response status", "status", resp.Status, "url", url)
+		spanRun.End()
+		return nil, err
 	}
+	spanRun.End()
 
 	// capture screenshot of an element
 	var buf []byte
 
+	reqCtx, spanShot := tracing.Tracer().Start(
+		reqCtx, "chromedp.Screenshot",
+	)
 	if err = chromedp.Run(ctx, chromedp.Tasks{
 		chromedp.WaitReady("#loaded", chromedp.NodeVisible),
 		chromedp.Sleep(100 * time.Millisecond),
 		chromedp.Screenshot(`#graph`, &buf, chromedp.NodeVisible),
 	}); err != nil {
-		slog.ErrorCtx(ctx, "screenshot", "err", err)
+		log.ErrorContext(reqCtx, "screenshot", "err", err)
+		spanShot.RecordError(err)
 	}
+	spanShot.End()
 
 	return buf, nil
 
